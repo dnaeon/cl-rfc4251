f3ec784ff413b8d168394796cf5a0b0b033a353c
Marin Atanasov Nikolov <dnaeon@gmail.com>
2020-07-16 10:28:53 +0300

    binary: commit generic function DECODE

--- src/binary.lisp
+++ src/binary.lisp
@@ -3,3 +3,6 @@
   (:use :cl)
   (:nicknames :openssh-cert.binary :ssh-cert.binary))
 (in-package :cl-openssh-cert.binary)
+
+(defgeneric decode (type stream &key)
+  (:documentation "Decode a value with the given type and stream" ))
