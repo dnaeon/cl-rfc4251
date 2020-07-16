7abda240da768240badbc367f418a214886d39c2
Marin Atanasov Nikolov <dnaeon@gmail.com>
2020-07-16 10:49:55 +0300

    binary: define DECODE method for reading raw bytes

--- src/binary.lisp
+++ src/binary.lisp
@@ -6,3 +6,13 @@
 
 (defgeneric decode (type stream &key)
   (:documentation "Decode a value with the given type and stream" ))
+
+(defmethod decode ((type (eql :raw-bytes)) stream &key (length 1))
+  "Read up to the given length of raw bytes from the stream"
+  (assert (plusp length) (length))
+  (let ((result (make-array length
+                            :element-type '(unsigned-byte 8)
+                            :fill-pointer 0)))
+    (loop repeat length do
+      (vector-push (read-byte stream) result))
+    result))
