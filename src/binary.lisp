bad91981788b2a1e4bc02c3220ae3c81af59ece9
Marin Atanasov Nikolov <dnaeon@gmail.com>
2020-07-18 22:42:37 +0300

    binary: add support for providing eof-value when decoding raw bytes

--- src/binary.lisp
+++ src/binary.lisp
@@ -9,12 +9,12 @@
 (defgeneric decode (type stream &key)
   (:documentation "Decode a value with the given type and stream" ))
 
-(defmethod decode ((type (eql :raw-bytes)) stream &key (length 1))
+(defmethod decode ((type (eql :raw-bytes)) stream &key (length 1) (eof-error-p t) eof-value)
   "Read up to the given length of raw bytes from the stream"
   (assert (plusp length) (length))
   (let ((result (make-array length
-                            :element-type '(unsigned-byte 8)
                             :fill-pointer 0)))
     (loop repeat length do
-      (vector-push (read-byte stream) result))
+      (vector-push (read-byte stream eof-error-p eof-value) result))
     result))
+
