ffba807002044563a93c01bad723b869b1907089
Marin Atanasov Nikolov <dnaeon@gmail.com>
2023-07-10 13:08:37 +0300

    Update test pipelines

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,7 +4,18 @@ set -e
 
 LISP=${LISP:-sbcl}
 
-${LISP} --eval '(ql:quickload :cl-rfc4251.test)' \
+_no_debug_flag=""
+case "${LISP}" in
+    sbcl)
+        _no_debug_flag="--non-interactive"
+        ;;
+    ecl)
+        _no_debug_flag="--nodebug"
+        ;;
+esac
+
+${LISP} ${_no_debug_flag} \
+        --eval '(ql:quickload :cl-rfc4251.test)' \
         --eval '(setf rove:*enable-colors* nil)' \
         --eval '(asdf:test-system :cl-rfc4251.test)' \
         --eval '(uiop:quit (length (rove/core/stats:all-failed-assertions rove/core/stats:*stats*)))'
